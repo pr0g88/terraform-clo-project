f5a4ffb6a36ffb417e5157e618dc29bd36fea8e0
pr0g88 <gutorov77@yandex.ru>
2026-08-05 23:29:51 +0300

    main: Added gitlab and gitlab runner servers

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "clo" {
   token    = var.clo_api_token
 }
 
+# NGINX
 resource "clo_compute_instance" "nginx" {
   name         = "nginx-server"
   flavor_vcpus = 1
@@ -34,9 +35,54 @@ resource "clo_compute_instance" "nginx" {
   }
 }
 
+# PostgreSQL
 resource "clo_compute_instance" "postgresql" {
   name         = "postgresql-server"
   flavor_vcpus = 1
+  flavor_ram   = 2
+  image_id     = var.default_image_id
+  project_id   = var.clo_project_id
+  keypairs     = var.default_keypair_ids
+
+  block_device {
+    bootable     = true
+    size         = 20
+    storage_type = "volume"
+  }
+
+  addresses {
+    external        = false
+    version         = 4
+    ddos_protection = false
+  }
+}
+
+# GitLab Server
+resource "clo_compute_instance" "gitlab" {
+  name         = "gitlab-server"
+  flavor_vcpus = 2
+  flavor_ram   = 4
+  image_id     = var.default_image_id
+  project_id   = var.clo_project_id
+  keypairs     = var.default_keypair_ids
+
+  block_device {
+    bootable     = true
+    size         = 80
+    storage_type = "volume"
+  }
+
+  addresses {
+    external        = false
+    version         = 4
+    ddos_protection = false
+  }
+}
+
+# GitLab Runner
+resource "clo_compute_instance" "gitlab_runner" {
+  name         = "gitlab-runner"
+  flavor_vcpus = 2
   flavor_ram   = 2
   image_id     = var.default_image_id
   project_id   = var.clo_project_id
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "nginx_id" {
 output "postgresql_id" {
   value = clo_compute_instance.postgresql.id
 }
+
+output "gitlab_id" {
+  value = clo_compute_instance.gitlab.id
+}
+
+output "gitlab_runner_id" {
+  value = clo_compute_instance.gitlab_runner.id
+}
